147fda1604be7d28896d1b0856931fc47a37673f
Samuel Henrique <samueel.jpg21@gmail.com>
2023-08-11 16:18:03 -0300

    .

---
+++ imperativa-declarativa.js
@@ -0,0 +1,13 @@
+//funcao que eleva o numero ao quadrado
+
+//imperativa: Faça da seguinte forma
+
+let numbeer = 8; //passo 1
+
+function square() { //passo 2
+    return number * number
+}
+number = square(); //passo 3
+console.log(number)
+
+//foi dado um passo a passo de como deve ser feito: a variavel number é declarada e atribuída o valor 8, logo depois a função square que retorna 
